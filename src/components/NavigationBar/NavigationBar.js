56931e5e932f2f39333a85499b70cf8c59f13e29
Petchie <53936910+PatrickPetch@users.noreply.github.com>
2020-08-24 02:38:18 +0800

    Update components

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavigationBar.scss'
+
+function NavigationBar() {
+    return (
+        <div className="navigation-bar">
+            
+        </div>
+    )
+}
+
+export default NavigationBar
