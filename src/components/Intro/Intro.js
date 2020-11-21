08ccd56ecd368ad21c63d7858c3368727f6ad800
PngmThu <phamthu0602@gmail.com>
2020-11-21 19:39:25 +0800

    Responsive text for intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -8,8 +8,7 @@ function Intro() {
         <h1>SEDS-NTU</h1>
         <div>Students for the</div>
         <div>Exploration and Development of Space</div>
-        <h2>_________________</h2>
-
+        <h2>______________</h2>
         <div>Nanyang Technological University, Singapore</div>
       </div>
     </div>
